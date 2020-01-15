82eb2578c2db9d9ac4d2c2f71ba113cb03066acb
Mitchell T Alton <mitchell.alton@live.com>
2020-01-15 17:32:01 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -36,4 +36,5 @@ def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
   aoh_update[1][:name] = new_value
+  aoh_update
 end
