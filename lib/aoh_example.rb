8eca2f7008d90dd4d2f834b9ba25266816450820
Mitchell T Alton <mitchell.alton@live.com>
2020-01-15 17:25:09 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -22,7 +22,9 @@ def literal_aoh
   literal_aoh = [
     {:name => "Don Gately", :occupation => "Live-in Staff"},
     {:name => "Joelle van Dyne", :occupation => "Radio Personality"},
-    
+    {:name => "Pat Monteseian", :occupation => "Staff"},
+    {:name => "Kate Gompert", :occupation => "None"},
+    {:name => "Bruce Green", :occupation => "Fan of Mildred"},
     ]
 end
 
