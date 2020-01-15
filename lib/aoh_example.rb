43037b891c8ac0c02073b431f7bf44220dced18a
Mitchell T Alton <mitchell.alton@live.com>
2020-01-15 17:29:41 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -35,5 +35,5 @@ end
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
-  assembled_aoh[1][:name] = "Joelle van Dyne (PGOAT)"
+  assembled_aoh[1][:name] = new_value
 end
